4b1e05b5f94aa6fab087720cae8795e8f42d37b7
zoovasoup <zulfa.plank@proton.me>
2025-05-30 12:18:43 +0700

    [FEAT] auth contreoller

--- src/main/java/com/Warung_CLI/Controllers/AuthController.java
+++ src/main/java/com/Warung_CLI/Controllers/AuthController.java
@@ -7,6 +7,19 @@ import com.Warung_CLI.Models.User;
  */
 public class AuthController {
 
+    public void loginMenu() {
+        System.out.println("Welcome to the Login Menu");
+        System.out.println("1. Login as Customer");
+        System.out.println("2. Login as Seller");
+        System.out.println("3. Exit");
+    }
+
+    public void registerMenu() {
+        System.out.println("Welcome to the Registration Menu");
+        System.out.println("1. Register as Customer");
+        System.out.println("2. Register as Seller");
+    }
+
     public void loginRedirect(User user) {
         if (user.isSeller()) {
         } else {
